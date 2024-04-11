e66f5bb06979f8f0c6d86f3eb3f804b47a33a6d9
Sala011 <160124031+Sala011@users.noreply.github.com>
2024-04-11 11:47:11 +0200

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+public class Main{
+    public static void main (String[]args) {
+        Scanner in= new Scanner(System.in);
+        int[][]m=new int[3][3];
+        boolean isPrimo= true;
+        for(int i=0;i<m.length;i++){
+            for(int j=0;j<m.length;j++){
+                System.out.println("inserisci un valore");
+                m[i][j]=in.nextInt();
+            }
+
+        }
+        System.out.println(isDiagonale(m,isPrimo));
+
+    }
+    private static boolean isDiagonale(int [][]m,boolean isPrimo){
+        for(int i=0;i<m.length;i++){
+            for(int j=0;j<m[0].length;j++){
+                if(m[i][j]!=m[j][i]){
+                    isPrimo=false;
+                    break;
+                }
+            }
+        }
+        return isPrimo;
+    }
+}
+
+
+
